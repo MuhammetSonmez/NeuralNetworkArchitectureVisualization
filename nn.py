6b20c619a80b63537da1ee0206780987447a5c59
Muhammet Sönmez <127973832+MuhammetSonmez@users.noreply.github.com>
2024-01-19 17:16:28 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -7,7 +7,9 @@ emb = [64/32, None, 256/32]
 gru = [128/32, None, 256/32]
 dense = [128/32, None, 65/32]
 
-layerList = [emb, gru, dense]
+emty = [1] # don't delete
+
+layerList = [emb, gru, dense, emty]
 
 layers = []
 
@@ -33,12 +35,6 @@ for a in range(len(layers)):
     x += [a for i in range(0, maximum, int(maximum/layers[a]))]
     y += [i  for i in range(0, maximum, int(maximum/layers[a]))]
 
-embNodes1 = []
-embNodes2 = []
-gruNodes1 = []
-gruNodes2 = []
-denseNodes1 = []
-denseNodes2 = []
 
 matrix = [x, y]
 rows, cols = len(matrix), len(matrix[0])
@@ -51,60 +47,47 @@ for i in range(rows):
 
 matrix = transposed_matrix
 
-embNodes1 = []
-embNodes2 = []
-gruNodes1 = []
-gruNodes2 = []
-denseNodes1 = []
-denseNodes2 = []
-
+maxy = 0
 for i in matrix:
-    if i[0] == 0:
-        embNodes1.append(i)
-    elif i[0] == 1:
-        embNodes2.append(i)
-    elif i[0] == 2:
-        gruNodes1.append(i)
-    elif i[0] == 3:
-        gruNodes2.append(i)
-    elif i[0] == 4:
-        denseNodes1.append(i)
-    elif i[0] == 5:
-        denseNodes2.append(i)
+    if i[0] > maxy:
+        maxy = i[0]
 
-for i in embNodes1:
-    for j in embNodes2:
-        x_emb = [i[0], j[0]]
-        y_emb = [i[1], j[1]]
 
-        plt.plot(x_emb, y_emb, marker="o", color="darkslateblue")
+nodeList = []
 
-for i in embNodes2:
-    for j in gruNodes1:
-        x_embGru = [i[0], j[0]]
-        y_embGru = [i[1], j[1]]
+for i in range(maxy):
+    row = []
+    for j in matrix:
+        if i == j[0]:
+            row.append(j)
+    nodeList.append(row)
 
-        plt.plot(x_embGru, y_embGru, marker="o", color="darkslateblue")
 
-for i in gruNodes1:
-    for j in gruNodes2:
-        x_gru = [i[0], j[0]]
-        y_gru = [i[1], j[1]]
+for l in range(len(nodeList)):
 
-        plt.plot(x_gru, y_gru, marker="o", color="darkslateblue")
+    list1 = nodeList[l-1]
+    list2 = nodeList[l]
 
-for i in gruNodes2:
-    for j in denseNodes1:
-        x_gruDense = [i[0], j[0]]
-        y_gruDense = [i[1], j[1]]
-        plt.plot(x_gruDense, y_gruDense, marker="o", color="darkslateblue")
+    edges = []
+    for i in list1:
+        row = []
+        for j in list2:
+            row.append([i, j])
+        edges.append(row)
 
-for i in denseNodes1:
-    for j in denseNodes2:
-        x_dense = [i[0], j[0]]
-        y_dense = [i[1], j[1]]
 
-        plt.plot(x_dense, y_dense, marker="o", color="darkslateblue")
+
+    for i in edges:
+        for j in i:
+            x = [j[0][0], j[1][0]]
+            y = [j[0][1], j[1][1]]
+            print(x,y)
+            plt.plot(x, y, marker="o", color="darkslateblue")
+
+print(matrix)
+
+
+
 
 plt.scatter([x], [y], marker='o', color="darkslateblue")
 
@@ -115,6 +98,6 @@ plt.ylabel('Nodes')
 plt.xticks([])
 plt.yticks([])
 
-plt.savefig("nerualNetworks.png")
+#plt.savefig("nerualNetworks.png")
 
-#plt.show()
+plt.show()
