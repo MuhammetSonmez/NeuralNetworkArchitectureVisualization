7f37b99ae28fd992c1e1c253b1493bc85da7aff4
Muhammet Sönmez <127973832+MuhammetSonmez@users.noreply.github.com>
2024-01-22 22:13:14 +0300

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,103 +1,146 @@
 import matplotlib.pyplot as plt
 
-plt.style.use('dark_background')
+def draw_model(layerList, save=False, show=False):
+
+    plt.style.use('dark_background')
+    layers = []
+
+    for i in range(len(layerList)):
+        for j in range(len(layerList[i])):
+            if j == 1:
+                continue
+            layers.append(layerList[i][j])
+
+    layers = dict(enumerate(layers))
+
+    maximum = 0
+    x = []
+    y = []
+
+    for a in range(len(layers)):
+        if layers[a] > maximum:
+            maximum = layers[a]
+
+    maximum = int(maximum)
+
+    for a in range(len(layers)):
+        x += [a for i in range(0, maximum, int(maximum/layers[a]))]
+        y += [i  for i in range(0, maximum, int(maximum/layers[a]))]
 
 
-emb = [64/32, None, 256/32]
-gru = [128/32, None, 256/32]
-dense = [128/32, None, 65/32]
+    matrix = [x, y]
+    rows, cols = len(matrix), len(matrix[0])
 
-emty = [1] # don't delete
+    transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
 
-layerList = [emb, gru, dense, emty]
+    for i in range(rows):
+        for j in range(cols):
+            transposed_matrix[j][i] = matrix[i][j]
 
-layers = []
+    matrix = transposed_matrix
 
-for i in range(len(layerList)):
-    for j in range(len(layerList[i])):
-        if j == 1:
-            continue
-        layers.append(layerList[i][j])
-
-layers = dict(enumerate(layers))
-
-maximum = 0
-x = []
-y = []
-
-for a in range(len(layers)):
-    if layers[a] > maximum:
-        maximum = layers[a]
-
-maximum = int(maximum)
-
-for a in range(len(layers)):
-    x += [a for i in range(0, maximum, int(maximum/layers[a]))]
-    y += [i  for i in range(0, maximum, int(maximum/layers[a]))]
+    maxy = 0
+    for i in matrix:
+        if i[0] > maxy:
+            maxy = i[0]
 
 
-matrix = [x, y]
-rows, cols = len(matrix), len(matrix[0])
+    nodeList = []
 
-transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
-
-for i in range(rows):
-    for j in range(cols):
-        transposed_matrix[j][i] = matrix[i][j]
-
-matrix = transposed_matrix
-
-maxy = 0
-for i in matrix:
-    if i[0] > maxy:
-        maxy = i[0]
-
-
-nodeList = []
-
-for i in range(maxy):
-    row = []
-    for j in matrix:
-        if i == j[0]:
-            row.append(j)
-    nodeList.append(row)
-
-
-for l in range(len(nodeList)):
-
-    list1 = nodeList[l-1]
-    list2 = nodeList[l]
-
-    edges = []
-    for i in list1:
+    for i in range(maxy):
         row = []
-        for j in list2:
-            row.append([i, j])
-        edges.append(row)
+        for j in matrix:
+            if i == j[0]:
+                row.append(j)
+        nodeList.append(row)
+
+
+    for l in range(1,len(nodeList) ,1):
+
+        list1 = nodeList[l-1]
+        list2 = nodeList[l]
+
+        
+        edges = []
+        
+        for i in list1:
+            row = []
+            for j in list2:
+                row.append([i, j])
+            edges.append(row)
 
 
 
-    for i in edges:
-        for j in i:
-            x = [j[0][0], j[1][0]]
-            y = [j[0][1], j[1][1]]
-            print(x,y)
-            plt.plot(x, y, marker="o", color="darkslateblue")
+        for i in edges:
+            for j in i:
+                x = [j[0][0], j[1][0]]
+                y = [j[0][1], j[1][1]]
+                #print(x,y)
+                plt.plot(x, y, marker="o", color="darkslateblue")
 
-print(matrix)
+    #print(matrix)
+    plt.scatter([x], [y], marker='o', color="darkslateblue")
+
+    plt.title('NerualNetworks')
+    plt.xlabel('Layers')
+    plt.ylabel('Nodes')
+
+    plt.xticks([])
+    plt.yticks([])
+
+    if save:
+        plt.savefig("nerualNetworks.png")
+
+    if show:
+        plt.show()
 
 
+def layer_parser(summary):
+    lines = summary.split("\n")
+    
+    layerList = []
+
+    for line in lines:
+        if "None" in line:
+            data = list(set(line.split("  ")))
+            for i in data:
+                if "None" in i:
+                    layerList.append([int(i.replace(" ", "").split(",")[0].replace("(", "")),None,int(i.replace(" ", "").split(",")[-1].replace(")", ""))])
+
+    layerList.append([1])
+
+    return layerList
 
 
-plt.scatter([x], [y], marker='o', color="darkslateblue")
+def main():
+    #tensorflow model.summary()
+    summary = """
+    Model: "sequential_1"
+    _________________________________________________________________
+    Layer (type)                Output Shape              Param #   
+    =================================================================
+    embedding_1 (Embedding)     (8, None, 8)              xxx     
+                                                                    
+    gru_1 (GRU)                 (4, None, 8)           xxx   
+                                                                    
+    dense_1 (Dense)             (4, None, 4)             xxx     
+                                                                    
+    =================================================================
+    Total params: xxx (xx.xx MB)
+    Trainable params: xxx (xx.xx MB)
+    Non-trainable params: x (x.xx Byte)
+    _________________________________________________________________
+    """
 
-plt.title('NerualNetworks')
-plt.xlabel('Layers')
-plt.ylabel('Nodes')
 
-plt.xticks([])
-plt.yticks([])
+    layerList = layer_parser(summary)
+    # or
+    # layer1 = [8, None, 8]
+    # layer2 = [4,None,8]
+    # layer3 = [4,None,4]
+    # empty = [1] # don't delete
+    # layerList = [layer1,layer2,layer3,empty]
 
-#plt.savefig("nerualNetworks.png")
+    draw_model(layerList=layerList, show= True)
 
-plt.show()
+main()
